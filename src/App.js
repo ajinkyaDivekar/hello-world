354b756b893654aa8ac12e4eb93d40cf593db422
ajinkyaDivekar <ajinkyadivekar363@gmail.com>
2019-07-15 20:38:09 +0530

    update 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './app.scss';
-import DashboardRoutes from 'component/dashboard';
-import Footer from './component/footer';
-import Header from './component/header';
+import DashboardRoutes from 'component/dashboard/index.js';
+import Footer from './component/footer.js';
+import Header from './component/header.js';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './route.js';
 class App extends Component {
